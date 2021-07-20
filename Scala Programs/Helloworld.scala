589af2f1baa2f5e556408af160647724f2edc37b
Piyulgaur <60064817+Piyulgaur@users.noreply.github.com>
2021-07-20 18:18:15 +0530

    Add files via upload

---
+++ Scala Programs/Helloworld.scala
@@ -0,0 +1,7 @@
+
+
+object Helloworld {
+  def main (args : Array[String]){
+    println("Hello World !")
+  }
+}
---
+++ Scala Programs/VarDeclaration.scala
@@ -0,0 +1,15 @@
+
+
+object VarDeclaration {
+  def main(args: Array[String]){
+    var myVar : Int = 10;
+    val myVal : String = "Hello scala with datatype declaration.";
+    var myVar1 = 20;
+    val myVal1 = "Hello scala new without datatype declaration.";
+    
+    println(myVar);
+    println(myVal);
+    println(myVar1);
+    println(myVal1);
+  }
+}
